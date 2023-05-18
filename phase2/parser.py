8e0484f9bf5866e9c99a1b3fb9e1e3fcd69890c1
ASUS <pro.alir.pro@gmail.com>
2023-05-18 22:52:34 +0330

    production 10 to 20

--- phase2/parser.py
+++ phase2/parser.py
@@ -1,4 +1,6 @@
 import json
+import anytree
+from anytree import Node, RenderTree
 
 from phase1.scanner import Scanner
 from phase1.utils.characterchecker import keywords_set
@@ -13,9 +15,9 @@ class Parser:
     char = None
     error_messages = dict()
     symbols = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<', '/', '*', '=', '=='}
+    parse_tree = None
 
     def __init__(self, scanner_location, predictset_location):
-
         self.scanner = Scanner(scanner_location)
         with open(predictset_location) as f:
             self.data = json.load(f)
@@ -25,12 +27,14 @@ class Parser:
             self.lookahead = self.scanner.get_next_token()
             if self.lookahead == '$':
                 print(self.error_messages)
+                # print(RenderTree(parse_tree))
                 break
             if self.lookahead is not None:
                 self.lookahead = str(self.lookahead)
                 self.set_token(self.lookahead)
                 self.set_char(self.lookahead)
                 self.set_line_no(self.lookahead)
+                # self.parse_tree = Node('Program')
                 self.program()
 
     def get_next(self):
@@ -84,7 +88,8 @@ class Parser:
     def illegal_error_message(self):
         if self.is_keyword(self.char) or self.is_symbol(self.char):
             return 'illegal ' + self.char
-        else: return 'illegal ' + self.token
+        else:
+            return 'illegal ' + self.token
 
     def missing_error_message(self):
         if self.is_keyword(self.char) or self.is_symbol(self.char):
@@ -106,6 +111,7 @@ class Parser:
 
     def program(self):
         if self.check_char_in_first('Declaration-list'):
+            # self.parse_tree = Node('Declaration-list')
             self.declaration_list()
         else:
             if self.non_terminal_panic_mode('Program'):
@@ -176,15 +182,138 @@ class Parser:
                 self.type_specifier()
 
     def params(self):
-        pass
+        if self.char == 'int':
+            self.match('int')
+            self.match('ID')
+            self.param_prime()
+            self.param_list()
+        elif self.char == 'void':
+            self.match('void')
+        else:
+            if self.non_terminal_panic_mode('Params'):
+                self.params()
+
+    def param_list(self):
+        if self.char == ',':
+            self.match(',')
+            self.param()
+            self.param_list()
+        else:
+            if self.non_terminal_panic_mode('Param-list'):
+                self.param_list()
+
+    def param(self):
+        if self.check_char_in_first('Declaration-initial'):
+            self.declaration_initial()
+            self.param_prime()
+        else:
+            if self.non_terminal_panic_mode('Param'):
+                self.param()
+
+    def param_prime(self):
+        if self.char == '[':
+            self.match('[')
+            self.match(']')
+        else:
+            if self.non_terminal_panic_mode('Param-prime'):
+                self.param_prime()
 
     def compound_stmt(self):
+        if self.char == '{':
+            self.match('{')
+            self.declaration_list()
+            self.statement_list()
+            self.match('}')
+        else:
+            if self.non_terminal_panic_mode('Compound-stmt'):
+                self.compound_stmt()
+
+    def statement_list(self):
+        if self.check_char_in_first('Statement'):
+            self.statement()
+            self.statement_list()
+        else:
+            if self.non_terminal_panic_mode('Statement-list'):
+                self.statement_list()
+
+    def statement(self):
+        if self.check_char_in_first('Expression-stmt'):
+            self.expression_stmt()
+        elif self.check_char_in_first('Compound-stmt'):
+            self.compound_stmt()
+        elif self.check_char_in_first('Selection-stmt'):
+            self.selection_stmt()
+        elif self.check_char_in_first('Iteration-stmt'):
+            self.iteration_stmt()
+        elif self.check_char_in_first('Return-stmt'):
+            self.return_stmt()
+        else:
+            if self.non_terminal_panic_mode('Statement'):
+                self.statement()
+
+    def expression_stmt(self):
+        if self.check_char_in_first('Expression'):
+            self.expression()
+            self.match(';')
+        elif self.char == 'break':
+            self.match('break')
+            self.match(';')
+        elif self.char == ';':
+            self.match(';')
+        else:
+            if self.non_terminal_panic_mode('Expression-stmt'):
+                self.expression_stmt()
+
+    def selection_stmt(self):
+        if self.char == 'if':
+            self.match('if')
+            self.match('(')
+            self.expression()
+            self.match(')')
+            self.statement()
+            self.match('else')
+            self.statement()
+        else:
+            if self.non_terminal_panic_mode('Selection-stmt'):
+                self.selection_stmt()
+
+    def iteration_stmt(self):
+        if self.char == 'repeat':
+            self.match('repeat')
+            self.statement()
+            self.match('until')
+            self.match('(')
+            self.expression()
+            self.match(')')
+        else:
+            if self.non_terminal_panic_mode('Iteration-stmt'):
+                self.iteration_stmt()
+
+    def return_stmt(self):
+        if self.char == 'return':
+            self.match('return')
+            self.return_stmt_prime()
+        else:
+            if self.non_terminal_panic_mode('Return-stmt'):
+                self.return_stmt()
+
+    def return_stmt_prime(self):
+        if self.char == ';':
+            self.match(';')
+        elif self.check_char_in_first('Expression'):
+            self.expression()
+            self.match(';')
+        else:
+            if self.non_terminal_panic_mode('Return-stmt-prime'):
+                self.return_stmt_prime()
+
+    def expression(self):
         pass
 
     def match(self, terminal):
         # if self.char == terminal:
         #     self.get_next()
-            # build tree
+        # build tree
         # else:
         #     self.add_error_message(self.missing_error_message())
         if self.is_keyword(self.char) or self.is_symbol(self.char):
@@ -200,7 +329,6 @@ class Parser:
                 # build tree
             else:
                 self.add_error_message('missing ' + terminal)
-
         self.get_next()
 
     def is_keyword(self, v):
