99a21837d8cffa3667ba3051bf894d9d05b7391e
ASUS <pro.alir.pro@gmail.com>
2023-05-18 16:10:09 +0330

    json

--- phase2/compiler.py
+++ phase2/compiler.py
@@ -4,4 +4,4 @@ from phase2.parser import Parser
 if __name__ == '__main__':
     input_file_path = "./input.txt"
 
-    parser = Parser(input_file_path, '')
+    parser = Parser(input_file_path, './utils/predictset.json')
