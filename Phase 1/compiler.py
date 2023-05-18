38ccab2fab50bedabdd0d38dfce65ea834941598
Alireza-Alipanah <alipanah.alireza@gmail.com>
2023-05-18 14:53:55 +0330

    added __name__ == '__main__' to phase 1 compiler

--- Phase 1/compiler.py
+++ Phase 1/compiler.py
@@ -1,20 +1,20 @@
 from scanner import Scanner
 from utils.scanneroutput import *
 
+if __name__ == '__main__':
+    input_file_path = "./input.txt"
+    token_file_path = "./tokens.txt"
+    lexical_error_file_path = "./lexical_errors.txt"
+    symbol_table_file_path = "./symbol_table.txt"
 
-input_file_path = "./input.txt"
-token_file_path = "./tokens.txt"
-lexical_error_file_path = "./lexical_errors.txt"
-symbol_table_file_path = "./symbol_table.txt"
+    scanner = Scanner(input_file_path)
 
-scanner = Scanner(input_file_path)
+    while True:
+        try:
+            scanner.get_next_token()
+        except EOFError:
+            break
 
-while True:
-    try:
-        scanner.get_next_token()
-    except EOFError:
-        break
-
-write_tokens(token_file_path, scanner.tokens)
-write_lexical_errors(lexical_error_file_path, scanner.error_messages)
-write_symbol_table(symbol_table_file_path, scanner.symbol_table)
+    write_tokens(token_file_path, scanner.tokens)
+    write_lexical_errors(lexical_error_file_path, scanner.error_messages)
+    write_symbol_table(symbol_table_file_path, scanner.symbol_table)
